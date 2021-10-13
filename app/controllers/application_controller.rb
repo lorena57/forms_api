e371c9f52bba77584960183000705d93807f5603
Lorena C <lorena@eunknown.org>
2021-10-12 20:54:21 -0700

    add JWT functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,19 @@ class ApplicationController < ActionController::API
     def encode_token(payload)
         JWT.encode(payload, 'my_secret')
     end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'my_secret', true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                []
+            end
+        end
+    end
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -11,4 +11,23 @@ class AuthController < ApplicationController
             end
     end
 
+    def auto_login
+        if session_user
+            render json: session_user
+        else
+            render json: {errors: 'No User Logged In'}
+        end
+    end
+
+    def session_user
+        decoded_hash = decoded_token
+        if !decoded_hash.empty?
+            user_id = decoded_hash[0]['user_id']
+            @user = User.find_by(id:user_id)
+        else
+            nil
+        end
+    end
+
+
 end
