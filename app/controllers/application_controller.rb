054581aa567c1851393f9bdeac1ab9b582795c67
Lorena C <lorena@eunknown.org>
2021-10-12 21:01:34 -0700

    move function from auth controller to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,19 @@ class ApplicationController < ActionController::API
             end
         end
     end
+
+    def session_user
+        decoded_hash = decoded_token
+        if !decoded_hash.empty?
+            user_id = decoded_hash[0]['user_id']
+            @user = User.find_by(id:user_id)
+        else
+            nil
+        end
+    end
+
+    def logged_in?
+        !!session_user
+    end
+
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -19,15 +19,7 @@ class AuthController < ApplicationController
         end
     end
 
-    def session_user
-        decoded_hash = decoded_token
-        if !decoded_hash.empty?
-            user_id = decoded_hash[0]['user_id']
-            @user = User.find_by(id:user_id)
-        else
-            nil
-        end
-    end
+
 
 
 end
