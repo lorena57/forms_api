abf6ecbfa5d15339e02bc89f5ee333a36d4ac9e2
Lorena C <lorena@eunknown.org>
2021-10-23 21:10:41 -0700

    update user controller to reflect schema

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -23,7 +23,7 @@ class Api::V1::UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:email, :name, :password)
     end
 
 end
